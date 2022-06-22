7df1e533aca19ffad80d5410b1f3a8d15fe3e854
pjh2104 <pjh2104@gmail.com>
2022-06-23 02:01:39 +0900

    솔루션 생성

---
+++ Src/StartDock/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using static System.Net.WebRequestMethods;
+
+namespace StartDock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // https://s-engineer.tistory.com/291
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                //if (false == p.WaitForInputIdle())
+                if (p.MainWindowHandle == IntPtr.Zero)
+                {
+                    continue;
+                }
+                Debug.WriteLine(p.Id + " " + p.ProcessName + " " + p.MainWindowTitle);
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
